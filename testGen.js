53388039bd6262b39abc72e1b06dbbc20c184459
Rahul Pandey <rrkrp100@gmail.com>
2023-04-26 08:56:49 +0530

    restored reader

--- testGen.js
+++ testGen.js
@@ -2,43 +2,43 @@ const generatorFunction = require("./testgenFunc");
 const fs = require("fs");
 const process = require("process");
 
-const reader = (source) => {
-  let treeData = generatorFunction.htmlToJson(source);
-  fs.writeFileSync("./htmljson.json", JSON.stringify(treeData), "utf-8");
-  console.log("done");
-};
-
-// const reader = async () => {
-//   console.time("completed");
-
-//   try {
-//     const [_, __, file] = process.argv;
-
-//     const dataMap = fs.readFileSync(file, "utf-8");
-//     let Map = JSON.parse(dataMap);
-//     for (const element of Map) {
-//       let [key, value] = element.split(" => ");
-//       const outPath = key.split("/").slice(2, -1).join("/");
-//       const filename = key.split("/").slice(-1);
-//       let treeData = generatorFunction.htmlToJson(key);
-//       console.log("json Generated");
-//       let rephrasedData = await generatorFunction.paraphrase(
-//         value,
-//         outPath,
-//         filename
-//       );
-//       console.log("rephrasing done");
-//       const generatedData = generatorFunction.findNestedObj(
-//         treeData,
-//         rephrasedData
-//       );
-//       await generatorFunction.writeHtmlFile(generatedData, outPath, filename);
-//     }
-//     console.timeEnd("completed");
-//   } catch (error) {
-//     console.log(error.message);
-//     console.timeEnd("completed");
-//   }
+// const reader = (source) => {
+//   let treeData = generatorFunction.htmlToJson(source);
+//   fs.writeFileSync("./htmljson.json", JSON.stringify(treeData), "utf-8");
+//   console.log("done");
 // };
+
+const reader = async () => {
+  console.time("completed");
+
+  try {
+    const [_, __, file] = process.argv;
+
+    const dataMap = fs.readFileSync(file, "utf-8");
+    let Map = JSON.parse(dataMap);
+    for (const element of Map) {
+      let [key, value] = element.split(" => ");
+      const outPath = key.split("/").slice(2, -1).join("/");
+      const filename = key.split("/").slice(-1);
+      let treeData = generatorFunction.htmlToJson(key);
+      console.log("json Generated");
+      let rephrasedData = await generatorFunction.paraphrase(
+        value,
+        outPath,
+        filename
+      );
+      console.log("rephrasing done");
+      const generatedData = generatorFunction.findNestedObj(
+        treeData,
+        rephrasedData
+      );
+      await generatorFunction.writeHtmlFile(generatedData, outPath, filename);
+    }
+    console.timeEnd("completed");
+  } catch (error) {
+    console.log(error.message);
+    console.timeEnd("completed");
+  }
+};
 
 reader();
