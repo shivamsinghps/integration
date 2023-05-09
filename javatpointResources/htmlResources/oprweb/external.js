005a319b38608b739c444c2fb9e3df8e98d6ae18
Shivam Singh <shivamsinghps@gmail.com>
2023-05-09 01:52:20 +0000

    added respurces

---
+++ javatpointResources/htmlResources/oprweb/external.js
@@ -0,0 +1,1 @@
+<script>function fun(){var x=document.getElementById("frm1").value;alert("Hi"+" "+x+"you have successfully submitted the details");}</script>
---
+++ javatpointResources/htmlResources/oprweb/myscript.js
@@ -0,0 +1,1 @@
+alert(" Hello World. \n Welcome to the javaTpoint.com \n This is an example of the defer attribute. ");
