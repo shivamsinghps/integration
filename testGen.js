4c73fa5c84e81e60467819408d132817b201a7db
Rahul Pandey <rrkrp100@gmail.com>
2023-05-15 00:49:05 +0530

    updaetd

--- testGen.js
+++ testGen.js
@@ -10,31 +10,33 @@ const process = require("process");
 
 const reader = async () => {
   try {
-    const [_, __, file] = process.argv;
-    console.time("completed");
-    console.log(file, "started");
-    const dataMap = fs.readFileSync(file, "utf-8");
-    let Map = JSON.parse(dataMap);
-    for (const element of Map) {
-      let [key, value] = element.split(" => ");
-      const outPath = key.split("/").slice(2, -1).join("/");
-      const filename = key.split("/").slice(-1);
-      let treeData = generatorFunction.htmlToJson(key);
-      console.log("json Generated");
-      let rephrasedData = await generatorFunction.paraphrase(
-        value,
-        outPath,
-        filename
-      );
-      console.log("rephrasing done");
-      const generatedData = generatorFunction.findNestedObj(
-        treeData,
-        rephrasedData
-      );
-      await generatorFunction.writeHtmlFile(generatedData, outPath, filename);
+    for (let fileno = 3; fileno <= 15; fileno++) {
+      let file = `./prepdata33/chunk${fileno}.txt`;
+      console.time("completed");
+      console.log(file, "started");
+      const dataMap = fs.readFileSync(file, "utf-8");
+      let Map = JSON.parse(dataMap);
+      for (const element of Map) {
+        let [key, value] = element.split(" => ");
+        const outPath = key.split("/").slice(2, -1).join("/");
+        const filename = key.split("/").slice(-1);
+        let treeData = generatorFunction.htmlToJson(key);
+        console.log("json Generated");
+        let rephrasedData = await generatorFunction.paraphrase(
+          value,
+          outPath,
+          filename
+        );
+        console.log("rephrasing done");
+        const generatedData = generatorFunction.findNestedObj(
+          treeData,
+          rephrasedData
+        );
+        await generatorFunction.writeHtmlFile(generatedData, outPath, filename);
+      }
+      console.log(file, "processed");
+      console.timeEnd("completed");
     }
-    console.log(file, "processed");
-    console.timeEnd("completed");
   } catch (error) {
     console.log({ mainerror: error });
     console.timeEnd("completed");
