33195bcc73a4517d6abbef8446d988af74a56275
Rahul Pandey <rrkrp100@gmail.com>
2023-05-23 18:20:22 +0530

    updated downloading scripts

---
+++ testDownload.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const process = require("process");
+const mkdirp = require("mkdirp");
+const https = require("https");
+
+const downloadandWriteAsset = async (url = null, dir, path, filename) => {
+  let localpath = await getPath(dir, path, filename);
+  https.get(url, (response) => {
+    response.pipe(localpath);
+  });
+};
+
+const getPath = async (dir, path, filename) => {
+  await mkdirp(dir + path);
+  return fs.createWriteStream(dir + path + "/" + filename);
+};
+
+const processAssets = async () => {
+  const data = fs.readFileSync("./demo2/Assets.txt", "utf-8");
+  let assetList = data.split("\n");
+  for (let i = 0; i < assetList.length; i++) {
+    let newPath = assetList[i].replace("https://static.javatpoint.com/", "");
+    let values = newPath.split("/");
+    console.log({ values, newPath, i });
+    let filename = values[values.length - 1];
+    let path = values.slice(0, -1).join("/");
+    await downloadandWriteAsset(assetList[i], "./Assets/", path, filename);
+  }
+};
+
+processAssets();
---
+++ testassets.js
@@ -0,0 +1,52 @@
+const glob = require("glob");
+const fs = require("fs");
+const process = require("process");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const mkdirp = require("mkdirp");
+
+let setfiles = async () => {
+  const [_, __, sourceHTMLs, outPath] = process.argv;
+  let HTMLfiles = await glob(`${sourceHTMLs}/**/*.html`);
+  for (const item of HTMLfiles) {
+    await processHtmlToText(item, outPath);
+  }
+  console.log("Completed");
+};
+
+const processHtmlToText = async (
+  inputFile = "./result/adobe-interview-questions.html",
+  outPath = "./demo2"
+) => {
+  try {
+    let result = "";
+    let data = fs.readFileSync(inputFile, "utf-8");
+    let dom = new JSDOM(data);
+
+    let imgblocks = dom.window.document.querySelectorAll("img");
+
+    imgblocks.forEach((val) => {
+      let data = val.getAttribute("src");
+      if (data !== null && data.includes("http")) {
+        result = result + val.getAttribute("src") + "\n";
+      }
+    });
+    await writeFile(outPath, "Assets", result);
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+};
+
+const writeFile = async (path, filename, content) => {
+  try {
+    await mkdirp(path);
+    fs.appendFileSync(path + "/" + filename + ".txt", content, "utf-8");
+    return;
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+};
+
+setfiles();
